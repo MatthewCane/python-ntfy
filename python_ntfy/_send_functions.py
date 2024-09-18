e034de2f693842997109cf2a61e9d2bcb84c1c42
Cody Wilson <cody@codywilson.co>
2024-09-18 17:07:14 -0400

    add support for tags and priority

--- python_ntfy/_send_functions.py
+++ python_ntfy/_send_functions.py
@@ -1,22 +1,39 @@
 import json, requests
+from enum import Enum
+from typing import Optional
 
+class MessagePriority(Enum):
+    """
+    Ntfy message priority levels.
+    """
+    MIN = 1
+    LOW = 2
+    DEFAULT = 3
+    HIGH = 4
+    MAX = 5
+    URGENT = MAX
+    
 
-def send(self, message: str, title: str = None, format_as_markdown: bool = False):
+def send(self, message: str, title: str = None, priority: Optional[MessagePriority] = MessagePriority.DEFAULT, tags: list = [], format_as_markdown: bool = False):
     """
     Send a text based message to the server
 
     :param message: The message to send
     :param title: The title of the message. Optional
+    :param priority: The priority of the message. Optional, defaults to MessagePriority.DEFAULT
+    :param tags: A list of tags to attach to the message. Can be an emoji short code. Optional
     :param format_as_markdown: If true, the message will be formatted as markdown. Optional
     :return: The response from the server
 
     :examples:
     response = client.send(message="Example message")
-    response = client.send(message="Example message", title="Example title")
+    response = client.send(message="Example message", title="Example title", priority=MessagePriority.HIGH, tags=["fire", "warning"])
     response = client.send(message="*Example markdown*", format_as_markdown=True)
     """
     headers = {
         "Title": title,
+        "Priority": priority.value,
+        "Tags": ",".join(tags),
         "Markdown": "true" if format_as_markdown else "false",
     }
 
@@ -26,12 +43,14 @@ def send(self, message: str, title: str = None, format_as_markdown: bool = False
     return response
 
 
-def send_file(self, file: str, title: str = None):
+def send_file(self, file: str, title: str = None, priority: Optional[MessagePriority] = MessagePriority.DEFAULT, tags: list = []):
     """
     Send a file to the server
 
-    :param file_path: The path to the file to send
+    :param file_path: The path to the file to send. 
     :param title: The title of the file. Optional
+    :param priority: The priority of the message. Optional, defaults to MessagePriority.DEFAULT
+    :param tags: A list of tags to attach to the message. Can be an emoji short code. Optional
     :return: The response from the server
 
     :examples:
