a4c0811064a846d06b4bc5697539fd17eec5ca80
PetrusBellmonte <petrusbellmonte@gmail.com>
2026-03-31 20:29:39 +0200

    Add MessageReceiveError exception and update get_cached_messages use it

--- python_ntfy/_exceptions.py
+++ python_ntfy/_exceptions.py
@@ -1,5 +1,5 @@
-class MessageSendError(Exception):
-    """Exception raised when a message fails to send."""
+class NtfyMessageError(Exception):
+    """Base exception for ntfy message errors."""
 
     def __init__(self, message: str) -> None:
         """Initialize the exception.
@@ -9,3 +9,11 @@ class MessageSendError(Exception):
         """
         self.message = message
         super().__init__(self.message)
+
+
+class MessageSendError(NtfyMessageError):
+    """Exception raised when a message fails to send."""
+
+
+class MessageReceiveError(NtfyMessageError):
+    """Exception raised when a message fails to retrieved."""
